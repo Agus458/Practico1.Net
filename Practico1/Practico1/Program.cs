01db5312c966cf03e2a487ca2ad2db787421896d
Agus458 <80288121+Agus458@users.noreply.github.com>
2021-08-15 16:20:50 +0100

    Practico

---
+++ Practico1/Practico1/Program.cs
@@ -0,0 +1,96 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> Personas = new List<Persona>();
+            string opcion = "";
+
+            do
+            {
+                Menu();
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("-----< Personas >-----");
+
+                        Imprimir(Personas);
+
+                        Console.ReadLine();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("-----< Ingresar Persona >-----");
+                        Console.WriteLine("Nombre: ");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Apellido: ");
+                        string apellido = Console.ReadLine();
+                        Console.WriteLine("Documento: ");
+                        string documento = Console.ReadLine();
+                        Console.WriteLine("Fecha Nacimiento: ");
+                        string fecha = Console.ReadLine();
+
+                        Persona aux = new Persona(nombre, apellido, documento, DateTime.Parse(fecha));
+                        Personas.Add(aux);
+
+                        Console.WriteLine("Persona Creada");
+                        Console.ReadLine();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("-----< Filtrar >-----");
+                        Console.WriteLine("Nombre: ");
+                        string n = Console.ReadLine();
+                        Console.WriteLine("Documento: ");
+                        string d = Console.ReadLine();
+
+                        List<Persona> filtrada = Personas.Where(persona => persona.Nombre.StartsWith(n) && persona.Documento.StartsWith(d)).ToList();
+
+                        Imprimir(filtrada);
+
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.Clear();
+            } while (opcion != "4");
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("-----< PRACTICO 1 >-----");
+            Console.WriteLine("1. Listar Personas");
+            Console.WriteLine("2. Ingresar Persona");
+            Console.WriteLine("3. Filtrar");
+            Console.WriteLine("4. Salir");
+        }
+
+        private static void Imprimir(List<Persona> Personas)
+        {
+            if (Personas.Count() > 0)
+            {
+                Personas.ForEach(persona =>
+                    Console.WriteLine("Nombre: " + persona.Nombre + ", Apellido: " + persona.Apellido + ", Documento: " + persona.Documento + ", FechaNacimiento: " + persona.FechaNacimiento.ToString("d") + ", Edad: " + persona.DarEdad())
+                );
+            }
+            else
+            {
+                Console.WriteLine("No hay personas...");
+            }
+        }
+    }
+}
---
+++ Practico1/Shared/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Documento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        // Constructor
+        public Persona(string _Nombre, string _Apellido, string _Documento, DateTime _FechaNacimiento)
+        {
+            this.Nombre = _Nombre;
+            this.Apellido = _Apellido;
+            this.Documento = _Documento;
+            this.FechaNacimiento = _FechaNacimiento;
+        }
+
+        public int DarEdad()
+        {
+            if (DateTime.Now.Month > this.FechaNacimiento.Month || (DateTime.Now.Month == this.FechaNacimiento.Month && DateTime.Now.Day > this.FechaNacimiento.Day))
+            {
+                return DateTime.Now.Year - this.FechaNacimiento.Year;
+            }
+
+            return DateTime.Now.Year - this.FechaNacimiento.Year - 1;
+        }
+    }
+}
